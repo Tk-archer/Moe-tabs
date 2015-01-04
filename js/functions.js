063839f08711dae342e5170032e06154517dd060
Fszer <616516248@qq.com>
2015-01-04 15:54:07 +0800

    小修改

--- js/functions.js
+++ js/functions.js
@@ -4,17 +4,27 @@
  * License MIT | http://opensource.org/licenses/MIT
  */
 
-setimg = function(image) {
+setimg = function (image) {
     $('body').css({
         'background-image': 'url(' + image + ')'
     });
 };
-bgimg = function() {
+
+getimg = function () {
+    var images = ["001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg", "006.jpg", "007.jpg", "008.jpg"];
+    return "/img/bg/" + images[Math.floor(Math.random() * images.length)];
+    
+};
+
+bgimg = function () {
 
     type = localStorage["type"] ? localStorage["type"] : 3;
     if (type === '1') {
-
-        setimg(localStorage["img"]);
+        var res;
+        chrome.storage.local.get("img", function (result) {
+            res = result ? result['img'] : getimg();
+            setimg(res);
+        });
 
     } else if (type === '2') {
 
@@ -22,23 +32,25 @@ bgimg = function() {
         var urls = "http://desk.zol.com.cn/showpic/1920x1080_50" + rande + "_14.html";
         $.ajax({
             url: urls,
-            success: function(data, status) {
+            success: function (data) {
                 var er = document.createElement('div');
                 er.innerHTML = data;
                 setimg(er.firstChild.src);
+            },
+            error: function () {
+                setimg(getimg());
             },
             dateType: "html"
         });
 
     } else if (type === '3') {
 
-        var images = ["001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg", "006.jpg", "007.jpg", "008.jpg"];
-        var rands = "/img/bg/" + images[Math.floor(Math.random() * images.length)];
-        setimg(rands);
+        setimg(getimg());
 
     }
 };
-setDate = function(el) {
+
+setDate = function (el) {
     var today = new Date();
     var y = today.getFullYear();
     var mon = today.getMonth() + 1;
@@ -47,49 +59,50 @@ setDate = function(el) {
     el.innerHTML = y + "." + mon + "." + d;
     my_clock(document.getElementById('clock_div'), today);
 };
-my_clock = function(argument, today) {
+
+my_clock = function (argument, today) {
     var h = today.getHours();
     var m = today.getMinutes();
     m = m >= 10 ? m : ('0' + m);
     argument.innerHTML = h + ":" + m;
-    setTimeout(function() {
+    setTimeout(function () {
         today = new Date();
         my_clock(argument, today);
     }, 10000);
 };
-imgview = function(file) {
+imgview = function (file) {
 
     var img = window.webkitURL.createObjectURL(file);
     var str = "<img src='" + img + "'>";
     $("#preview").html(str);
 };
-getActive=function() {
-    type=localStorage["type"]?localStorage["type"]:3;
-        switch(type){
-            case '1':
-                return $('#one'); 
-            case '2':
-                 return $('#rand');          
-            case '3':
-                 return $('#nomal');
-            default:
-                    localStorage["type"]=type;
-                    return $('#nomal');          
-        }
+getActive = function () {
+    type = localStorage["type"] ? localStorage["type"] : 3;
+    switch (type) {
+        case '1':
+            return $('#one');
+        case '2':
+            return $('#rand');
+        case '3':
+            return $('#nomal');
+        default:
+            localStorage["type"] = type;
+            return $('#nomal');
+    }
 };
-changeButton=function  (el) {
-   var act=getActive();
-        var old=act.children("i")[0];
-        var news=old.cloneNode();
-        act.removeClass("am-active ");
-        old.remove();
-        el.addClass("am-active ");
-        el.append(news);
-        localStorage["type"]=el.val();    
+changeButton = function (el) {
+    var act = getActive();
+    var old = act.children("i")[0];
+    var news = old.cloneNode();
+    act.removeClass("am-active ");
+    old.remove();
+    el.addClass("am-active ");
+    el.append(news);
+    localStorage["type"] = el.val();
 };
 function init() {
 
-    chrome.topSites.get(function(topSitesArray) {
+    chrome.topSites.get(function (topSitesArray) {
         // set background
         bgimg();
         var sites = [],
@@ -115,32 +128,20 @@ function init() {
 
 
         $('ul#topSites').append(sites);
-        var sdd=getActive();
-        var sd='<i class=" am-icon-check-square"></i>';
-        sdd.addClass("am-active");
-        sdd.append(sd);
+        var act = getActive();
+        var icon = '<i class=" am-icon-check-square"></i>';
+        act.addClass("am-active");
+        act.append(icon);
     });
 };
-
-/*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message[0] == "img") {
-        localStorage[message[0]] = message[1];
-    }
-});
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message[0] == "type") {
-        localStorage[message[0]] = message[1];
-    }
-});*/
-
 
 init();
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     setDate(document.getElementById('date_div'));
     var fileList;
-    $('#imgurl').change(function(argument) {
+    $('#imgurl').change(function (argument) {
         fileList = document.getElementById('imgurl').files[0];
         if (fileList.type.indexOf("image") !== -1) {
             imgview(fileList);
@@ -151,22 +152,22 @@ $(document).ready(function() {
     });
 
     $(document).on({
-        dragleave: function(e) { //拖离 
+        dragleave: function (e) { //拖离
             e.preventDefault();
         },
-        drop: function(e) { //拖后放 
+        drop: function (e) { //拖后放
             e.preventDefault();
         },
-        dragenter: function(e) { //拖进 
+        dragenter: function (e) { //拖进
             e.preventDefault();
         },
-        dragover: function(e) { //拖来拖去 
+        dragover: function (e) { //拖来拖去
             e.preventDefault();
         }
     });
 
     var box = document.getElementById('drop_area'); //拖拽区域 
-    box.addEventListener("drop", function(e) {
+    box.addEventListener("drop", function (e) {
         e.preventDefault(); //取消默认浏览器拖拽效果 
         fileList = e.dataTransfer.files[0]; //获取文件对象 
 
@@ -177,7 +178,7 @@ $(document).ready(function() {
         }
     }, false);
 
-    $('#more').mousedown(function() {
+    $('#more').mousedown(function () {
         if ($("#option").css("display") === "none") {
             $('#option').slideDown("fast");
         } else {
@@ -185,25 +186,29 @@ $(document).ready(function() {
         }
     });
 
-    $('#up').mousedown(function() {
+    $('#up').mousedown(function () {
         if (fileList.type.indexOf("image") !== -1) {
             var reader = new FileReader();
             reader.readAsDataURL(fileList);
-            reader.onload = function() {
-                localStorage["img"] = reader.result;
-                location.reload();
-                
+            reader.onload = function () {
+
+                chrome.storage.local.set({"img": reader.result}, function () {
+                    location.reload();
+                });
+
+
             };
-        };
+        }
+        ;
     });
 
     $('#one').mousedown(function () {
         changeButton($(this));
     });
-     $('#rand').mousedown(function () {
+    $('#rand').mousedown(function () {
         changeButton($(this));
     });
-      $('#nomal').mousedown(function () {
+    $('#nomal').mousedown(function () {
         changeButton($(this));
     });
 });
